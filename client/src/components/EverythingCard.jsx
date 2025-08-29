a6c46d40fa4170b388bddb6c57c7e70b5009110e
Aaditya Aanand <147561897+Aaditya514@users.noreply.github.com>
2025-08-29 13:55:20 +0530

    Update EverythingCard.jsx

--- client/src/components/EverythingCard.jsx
+++ client/src/components/EverythingCard.jsx
@@ -2,54 +2,57 @@ import React from "react";
 
 function Card(props) {
   return (
-    <div className="everything-card mt-10">
-      <div className="everything-card flex flex-wrap p-5 gap-1 mb-1">
-        <b className="title">{props.title}</b>
+    <div className="everything-card mt-6 max-w-md"> {/* reduced margin & limited width */}
+      <div className="everything-card flex flex-wrap p-3 gap-1 mb-2 shadow-md rounded-lg">
+        <b className="title text-lg">{props.title}</b> {/* slightly smaller text */}
         <div className="everything-card-img mx-auto">
-          <img className="everything-card-img" src={props.imgUrl} alt="img" />
+          <img
+            className="everything-card-img max-h-40 object-cover rounded-md"
+            src={props.imgUrl}
+            alt="img"
+          />
         </div>
         <div className="description">
-          <p className="description-text leading-7">
-            {props.description?.substring(0, 200)}
+          <p className="description-text text-sm leading-6">
+            {props.description?.substring(0, 150)} {/* shorter description */}
           </p>
         </div>
-        <div className="info">
-          <div className="source-info flex items-center gap-2">
+        <div className="info mt-2">
+          <div className="source-info flex items-center gap-2 text-sm">
             <span className="font-semibold">Source:</span>
             <a
               href={props.url}
               target="_blank"
               className="link underline break-words"
             >
-              {props.source.substring(0, 70)}
+              {props.source.substring(0, 50)}
             </a>
           </div>
-          <div className="origin flex flex-col">
+          <div className="origin flex flex-col text-xs">
             <p className="origin-item">
-              <span className="font-semibold">Author:</span>
-              {props.author}
+              <span className="font-semibold">Author:</span> {props.author}
             </p>
             <p className="origin-item">
-              <span className="font-semibold">Published At:</span>
-              ({props.publishedAt})
+              <span className="font-semibold">Published At:</span>{" "}
+              {props.publishedAt}
             </p>
           </div>
         </div>
       </div>
 
-      {/* Added the new card content with styles */}
-      <div className="flex lg:flex-row">
+      {/* Compact secondary card */}
+      <div className="flex lg:flex-row shadow-sm rounded-lg overflow-hidden">
         <div
-          className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+          className="h-32 lg:h-auto lg:w-32 flex-none bg-cover text-center"
           style={{ backgroundImage: `url(${props.imageUrlLeft})` }}
           title={props.imageLeftTitle}
         ></div>
-        <div className="border rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-          <div className="mb-8">
-            <p className="text-sm text-gray-600 flex items-center">
+        <div className="border p-3 flex flex-col justify-between leading-snug">
+          <div className="mb-3">
+            <p className="text-xs text-gray-600 flex items-center">
               {props.memberIcon && (
                 <svg
-                  className="fill-current text-gray-500 w-3 h-3 mr-2"
+                  className="fill-current text-gray-500 w-3 h-3 mr-1"
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 20 20"
                 >
@@ -58,20 +61,20 @@ function Card(props) {
               )}
               {props.memberText}
             </p>
-            <div className="text-gray-900 font-bold text-xl mb-2">
+            <div className="text-gray-900 font-semibold text-base mb-1">
               {props.cardTitle}
             </div>
-            <p className="text-gray-700 text-base">{props.cardDescription}</p>
+            <p className="text-gray-700 text-sm">{props.cardDescription}</p>
           </div>
           <div className="flex items-center">
             {props.authorImage && (
               <img
-                className="w-10 h-10 rounded-full mr-4"
+                className="w-8 h-8 rounded-full mr-2"
                 src={props.authorImage}
                 alt="Avatar"
               />
             )}
-            <div className="text-sm">
+            <div className="text-xs">
               <p className="text-gray-900 leading-none">{props.authorName}</p>
               <p className="text-gray-600">{props.publishedDate}</p>
             </div>
